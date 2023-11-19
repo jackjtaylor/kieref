4e039bf82147e2c8ef93bd8594e50aa9e73e4efe
jackjtaylor <jackjt@outlook.com.au>
2023-11-19 23:08:57 +1000

    feat(classes): create RefcauObjectGeneric class

---
+++ src/classes.py
@@ -0,0 +1,38 @@
+from abc import ABC as AbstractBaseClass, abstractmethod
+from dataclasses import dataclass
+
+"""
+This file defines the classes used to create references.
+@author Jack Taylor
+@date 19/11/2023
+"""
+
+
+@dataclass
+class RefcauObjectGeneric(AbstractBaseClass):
+    """
+    This abstract dataclass is a base object for creating references.
+    """
+
+    name: str
+    creator: str
+    date: int
+
+    @abstractmethod
+    def reference(self) -> str:
+        """
+        This abstract method returns a reference based on the object.
+
+        :return: The source reference
+        :rtype: str
+        """
+        return f"{self.creator}, {self.name}. {self.date}."
+
+    def cite(self) -> str:
+        """
+        This method returns a citation based on the object.
+
+        :return: The source citation
+        :rtype: str
+        """
+        return f"({self.creator}, {self.date})"
