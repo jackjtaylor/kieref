779476f3a93528b7444cf3d24e99fa6e56d23314
jackjtaylor <jackjt@outlook.com.au>
2024-02-23 19:13:07 +1000

    feat(cli): add create() and get() function to cli.py
    note: some variables must be changed to not shadow built-ins

--- kieref/cli.py
+++ kieref/cli.py
@@ -1,23 +1,28 @@
 """
 This module is the text-based interface for Kieref.
 """
+from typing import Type
 
-from click import group, argument, echo, option, prompt
+from click import group, argument, echo, option
+from dataclasses import fields
+
 from manager import KierefManager
-from sources import KierefSourceElectronic, KierefSourceArticle
+from sources import KierefSourceArticle, KierefSourceElectronic, KierefSourceType
 
 __date__ = "04/01/2024"
 __author__ = "Jack Taylor"
 
 manager = KierefManager()
+types: dict[str, KierefSourceType] = {
+    "article": KierefSourceArticle,
+    "electronic": KierefSourceElectronic
+}
 
 
 @group("cli")
 def cli() -> None:
     """
-    This function is the main function of this module.
-
-    When using the 'cli' group, this function will display help to the user.
+    This application lets users create and manage sources.
     """
     pass
 
@@ -31,15 +36,39 @@ def sources() -> None:
 
 
 @cli.command()
+@option("--index", prompt="What index do you want to get?", type=int, required=True)
+def get(index: int) -> None:
+    """
+    This function gets a source based on its index.
+    """
+    source = manager.get_source(index)
+    print(f"Source {index}:")
+    print(f"Cite: {source.cite()}")
+    print(f"Reference: {source.reference()}")
+
+
+@cli.command()
 @option("--type", prompt="What source type do you want to create?")
 def create(type: str) -> None:
     """
     This function creates a new source based on user input.
     """
-    pass
+    type = type.strip().lower()
+
+    if type in types:
+        vars = {}
+        for field in fields(types[type]):
+            vars[field.name] = input(f"{field.name.capitalize()}: ")
+        source = types[type](**vars)
+        manager.add_source(source)
+        print(f"The source {source.title} was added.")
+        print(f"{source.id}")
+    else:
+        print("Invalid source type.")
+
 
 @cli.command()
-@argument('lang')
+@argument("lang")
 def language(lang) -> None:
     """
     This function changes the language interface.
